cb4dc9da24cf923b0855c9110ddbb34ac2e7736d
Arto Bendiken <arto@bendiken.net>
2026-09-04 05:21:24 -0700

    Implement `init_tracing_subscriber()`.

--- lib/clientele/src/tracing.rs
+++ lib/clientele/src/tracing.rs
@@ -1,9 +1,13 @@
 // This is free and unencumbered software released into the public domain.
 
+use crate::StandardOptions;
 use std::sync::LazyLock;
-use tracing_subscriber::fmt::format::{Compact, Format};
+use tracing_subscriber::fmt::{
+    format::{Compact, Format},
+    time::SystemTime,
+};
 
-pub const STDERR_FORMAT: LazyLock<Format<Compact, ()>> = LazyLock::new(|| {
+pub const STDERR_PLAIN_FORMAT: LazyLock<Format<Compact, ()>> = LazyLock::new(|| {
     tracing_subscriber::fmt::format()
         .compact()
         .without_time()
@@ -14,3 +18,19 @@ pub const STDERR_FORMAT: LazyLock<Format<Compact, ()>> = LazyLock::new(|| {
         .with_file(false)
         .with_line_number(false)
 });
+
+pub const STDERR_DEBUG_FORMAT: LazyLock<Format<Compact, ()>> =
+    LazyLock::new(|| tracing_subscriber::fmt::format().compact().without_time());
+
+/// Initializes `tracing_subscriber` based on the given options.
+pub fn init_tracing_subscriber(options: &StandardOptions) {
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_max_level(options)
+        .event_format(if options.debug {
+            STDERR_DEBUG_FORMAT.clone()
+        } else {
+            STDERR_PLAIN_FORMAT.clone()
+        })
+        .init();
+}
